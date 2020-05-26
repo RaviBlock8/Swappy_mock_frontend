89a143d8d5c1237c3f1c810fdbd120d719922169
RaviVerma <59726806+RaviBlock8@users.noreply.github.com>
2020-05-26 16:16:02 +0530

    Customized input number field (#4)
    * Connect select with useState
    
    * Customized number input
    
    Co-authored-by: RaviBlock8 <ravi83165@gmail.com>

--- src/pages/SwapPage/SwapPage.jsx
+++ src/pages/SwapPage/SwapPage.jsx
@@ -4,14 +4,34 @@ import SwapWindow from "../../components/SwapWindow/SwapWindow";
 function SwapPage() {
   const [tokenInType, setTokenInType] = useState("eth");
   const [tokenOutType, setTokenOutType] = useState("dai");
+  const [errorMsg, setErrorMsg] = useState("");
+  const regex = /^[0-9\b]+$/;
+  const changeTokenType = (tokenName, tokenType) => {
+    switch (tokenType) {
+      case "tokenIn":
+        setTokenInType(tokenName);
+        tokenName == tokenOutType
+          ? setErrorMsg("Token In and Token Out cannot be of same type")
+          : setErrorMsg("");
+        break;
+      case "tokenOut":
+        setTokenOutType(tokenName);
+        tokenName == tokenInType
+          ? setErrorMsg("Token In and Token Out cannot be of same type")
+          : setErrorMsg("");
+        break;
+      default:
+        setErrorMsg("Not correct type of tokens");
+    }
+  };
   return (
     <Grid container justify="center">
       <Grid item xs={12} md={7} style={{ marginTop: "20vh" }}>
         <SwapWindow
+          changeTokenType={changeTokenType}
+          errorMsg={errorMsg}
           tokenInType={tokenInType}
-          setTokenInType={setTokenInType}
           tokenOutType={tokenOutType}
-          setTokenOutType={setTokenOutType}
         />
       </Grid>
     </Grid>
