99073f1564708c6b42f4e7a558a887b771d852e2
Sahil1109 <sahil.jangra@block8.com>
2020-05-26 17:34:16 +0530

    Add validation on token type
    In this I added validation on token type while adding liquidity i.e token1 and token2 can't be of same type

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import SwapPage from "./pages/SwapPage/SwapPage";
+import AddLiquidityPage from "./pages/AddLiquidityPage/AddLiquidityPage";
 
 function App() {
-  return <SwapPage />;
+  return (
+    <React.Fragment>
+      <SwapPage />
+      <AddLiquidityPage />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/pages/AddLiquidityPage/AddLiquidityPage.jsx
+++ src/pages/AddLiquidityPage/AddLiquidityPage.jsx
@@ -3,13 +3,40 @@ import { Grid } from "@material-ui/core";
 import AddLiquidityWindow from "../../components/AddLiquidity/AddLiquidityWindow";
 
 function AddLiquidityPage() {
-	return (
-		<Grid container justify="center">
-			<Grid item xs={12} md={7} style={{ marginTop: "20vh" }}>
-				<AddLiquidityWindow/>
-			</Grid>
-		</Grid>
-	);
+  const [token1, setToken1] = React.useState("");
+  const [token2, setToken2] = React.useState("");
+  const [errorMsg, setErrorMsg] = React.useState("");
+
+  const changeTokenType = (tokenName, tokenType) => {
+    switch (tokenType) {
+      case "token1":
+        setToken1(tokenName);
+        tokenName == token2
+          ? setErrorMsg("Token 1 and Token 2 cannot be of same type")
+          : setErrorMsg("");
+        break;
+      case "token2":
+        setToken2(tokenName);
+        tokenName == token1
+          ? setErrorMsg("Token 1 and Token 2 cannot be of same type")
+          : setErrorMsg("");
+        break;
+      default:
+        setErrorMsg("Not correct type of tokens");
+    }
+  };
+  return (
+    <Grid container justify="center">
+      <Grid item xs={12} md={7} style={{ marginTop: "20vh" }}>
+        <AddLiquidityWindow
+          changeTokenType={changeTokenType}
+          errorMsg={errorMsg}
+          token1={token1}
+          token2={token2}
+        />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default AddLiquidityPage;
