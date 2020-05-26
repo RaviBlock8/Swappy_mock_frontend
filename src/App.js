dcbda6a561c4713f739a5f335a854cfe79a82776
RaviBlock8 <ravi83165@gmail.com>
2020-05-26 15:14:56 +0530

    Connect select with useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import SwapWindow from "./components/SwapWindow";
 import SwapPage from "./pages/SwapPage/SwapPage";
 
 function App() {
--- src/pages/SwapPage/SwapPage.jsx
+++ src/pages/SwapPage/SwapPage.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import SwapWindow from "../../components/SwapWindow/SwapWindow";
 function SwapPage() {
+  const [tokenInType, setTokenInType] = useState("eth");
+  const [tokenOutType, setTokenOutType] = useState("dai");
   return (
     <Grid container justify="center">
       <Grid item xs={12} md={7} style={{ marginTop: "20vh" }}>
-        <SwapWindow />
+        <SwapWindow
+          tokenInType={tokenInType}
+          setTokenInType={setTokenInType}
+          tokenOutType={tokenOutType}
+          setTokenOutType={setTokenOutType}
+        />
       </Grid>
     </Grid>
   );
