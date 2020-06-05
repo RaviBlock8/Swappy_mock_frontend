022029646c279725df5faecda03fe5716a63a512
Sahil1109 <sahil.jangra@block8.com>
2020-06-05 13:08:35 +0530

    Add History data graph for BTC

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddLiquidityPage from "./pages/AddLiquidityPage/AddLiquidityPage";
 import ToggleWindows from "./components/ToggleWindows/ToggleWindows";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NewLayout from "./pages/NewLayout/NewLayout";
+import CurrencyGraphPage from "./pages/CurrencyGraph/CurrencyGraphPage";
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route exact path="/new">
             <NewLayout />
+          </Route>
+          <Route exact path="/graph">
+            <CurrencyGraphPage />
           </Route>
         </Switch>
       </React.Fragment>
---
+++ src/pages/CurrencyGraph/CurrencyGraphPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CurrencyGraphWindow from '../../components/CurrencyGraphWindow/CurrencyGraphWindow'
+
+function CurrencyGraphPage() {
+    return (
+        <div>
+            <CurrencyGraphWindow />
+        </div>
+    )
+}
+
+export default CurrencyGraphPage;
